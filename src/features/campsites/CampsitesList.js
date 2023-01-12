4b21e251e15860e753f79b470aa4d76a875c6873
Colton Harmon <cmharmon89@gmail.com>
2023-01-12 09:56:47 -0800

    campsiteSlice

---
+++ src/features/campsites/CampsitesList.js
@@ -0,0 +1,22 @@
+import { Col, Row } from 'reactstrap';
+import CampsiteCard from "./CampsiteCard";
+import { selectAllCampsites } from './campsitesSlice';
+
+
+const CampsitesList = () => {
+    const campsites = selectAllCampsites();
+
+    return (
+        <Row className='ms-auto'>
+            {campsites.map((campsite) => { 
+                return (
+                    <Col className='m-4' md='5' key={campsite.id}>
+                        <CampsiteCard campsite={campsite} />
+                    </Col> 
+                );   
+            })}
+        </Row>
+    )
+}
+
+export default CampsitesList; 
