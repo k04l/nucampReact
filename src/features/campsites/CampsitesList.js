cdbb77b5067d5b404e5b81fd472ee3938ac7e27e
Colton Harmon <cmharmon89@gmail.com>
2023-01-12 17:58:00 -0800

    CampsiteDetailPage and useParams

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -3,26 +3,20 @@ import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from './campsitesSlice';
 
 
-const CampsitesList = ( {setCampsiteId} ) => {
+const CampsitesList = () => {
     const campsites = selectAllCampsites();
-
-        return (
-            <Row className='ms-auto'>
-                {campsites.map((campsite) => { 
-                    return (
-                        <Col 
-                            className='m-4'
-                            md='5'
-                            key={campsite.id}
-                            onClick={() => setCampsiteId(campsite.id)}
-                        >
-                            <CampsiteCard campsite={campsite} />
-                        </Col> 
-                    );   
-                })}
-            </Row>
-        )
-    }
+    return (
+        <Row className='ms-auto'>
+            {campsites.map((campsite) => {
+                return (
+                    <Col md='5' className='m-4' key={campsite.id}>
+                        <CampsiteCard campsite={campsite} />
+                    </Col>
+                );
+            })}
+        </Row>
+    );
+};
 
 
 export default CampsitesList; 
